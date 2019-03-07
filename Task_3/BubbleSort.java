a50ce25dbb49e3270818fc240f04a7d351c8be6d
Skye <lantian9494@163.com>
2019-03-07 20:00:43 +0800

    冒泡排序

--- Task_3/BubbleSort.java
+++ Task_3/BubbleSort.java
@@ -3,11 +3,11 @@ package DataWhale.Task3;
 public class BubbleSort {
     public static void bubbleSort(int arr[]) {
         for (int i = 0; i < arr.length; i++)
-            for (int j = 1; j < arr.length - 1 - i; j++)
-                if (arr[j + 1] < arr[j]) {
-                    int temp = arr[j + 1];
-                    arr[j + 1] = arr[j];
-                    arr[j] = temp;
+            for (int j = 0; j < arr.length - i - 1; j++)
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
     }
 
@@ -16,9 +16,8 @@ public class BubbleSort {
         long startTime1 = System.nanoTime();
         bubbleSort(a);
         long endTime1 = System.nanoTime();
-        for (int i = 0; i < a.length; i++) {
+        for (int i = 0; i < a.length; i++)
             System.out.print(a[i] + " ");
-        }
         long usedTime1 = (endTime1 - startTime1);
         System.out.println("bubbleSort: " + usedTime1 + " ns");
     }
