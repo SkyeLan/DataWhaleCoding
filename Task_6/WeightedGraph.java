8d19865ab8ce759dd4717ce7a12a9a72c78e3fc5
Skye <lantian9494@163.com>
2019-03-12 22:05:19 +0800

    带权图

---
+++ Task_6/WeightedGraph.java
@@ -0,0 +1,17 @@
+package DataWhale.Task6;
+
+import Algorithm_Struction.Graph.Edge;
+
+interface WeightedGraph<Weight extends Number & Comparable> {
+    public int V();
+
+    public int E();
+
+    public void addEdge(Edge<Weight> e);
+
+    boolean hasEdge(int v, int w);
+
+    void show();
+
+    public Iterable<Edge<Weight>> adj(int v);
+}
