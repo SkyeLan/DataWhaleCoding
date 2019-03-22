e9caae446d0b100dc44b99cec3e1d6a40b890fa8
Skye <lantian9494@163.com>
2019-03-22 12:52:51 +0800

    236. 二叉树的最近公共祖先

---
+++ Leetcode/_0236_LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,30 @@
+package DataWhale.Leetcode;
+
+/**
+ * Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+ * According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between
+ * two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a
+ * node to be a descendant of itself).”
+ * <p>
+ * Note:
+ * All of the nodes' values will be unique.
+ * p and q are different and both values will exist in the binary tree.
+ */
+public class _0236_LowestCommonAncestorOfABinaryTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null)
+            return null;
+        if (root == p || root == q)
+            return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null)
+            return root;
+        if (left == null)
+            return right;
+        if (right == null)
+            return left;
+        return null;
+    }
+}
