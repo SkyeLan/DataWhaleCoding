b36de70a6368f178ff471d325fbb8b0e3c8e9149
Skye <lantian9494@163.com>
2019-07-04 21:58:41 +0800

    Add files via upload

--- Leetcode/_0088_MergeSortedArray.java
+++ Leetcode/_0088_MergeSortedArray.java
@@ -10,19 +10,29 @@ package DataWhale.Leetcode;
  */
 public class _0088_MergeSortedArray {
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] temp = new int[m];
-        for (int i = 0; i < m; i++)
-            temp[i] = nums1[i];
-        int i = 0, j = 0, index = 0;
-        while (i < m && j < n)
-            if (temp[i] < nums2[j])
-                nums1[index++] = temp[i++];
+//        int[] temp = new int[m];
+//        for (int i = 0; i < m; i++)
+//            temp[i] = nums1[i];
+//        int i = 0, j = 0, index = 0;
+//        while (i < m && j < n)
+//            if (temp[i] < nums2[j])
+//                nums1[index++] = temp[i++];
+//            else
+//                nums1[index++] = nums2[j++];
+//        while (i < m)
+//            nums1[index++] = temp[i++];
+//        while (j < n)
+//            nums1[index++] = nums2[j++];
+        int index1 = m - 1, index2 = n - 1;
+        int index = nums1.length - 1;
+        while (index1 >= 0 && index2 >= 0) {
+            if (nums1[index1] < nums2[index2])
+                nums1[index--] = nums2[index2--];
             else
-                nums1[index++] = nums2[j++];
-        while (i < m)
-            nums1[index++] = temp[i++];
-        while (j < n)
-            nums1[index++] = nums2[j++];
+                nums1[index--] = nums1[index1--];
+        }
+        while (index2 >= 0)
+            nums1[index--] = nums2[index2--];
     }
 
     public static void main(String[] args) {
